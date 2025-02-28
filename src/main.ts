d27e31da0c6c932d3c15b914882510ce77ba8117
Quang Sơn Phạm <phamquangson4493@gmail.com>
2025-02-28 16:58:50 +0700

    set the host exception port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('PORT') || 3000;
   app.setGlobalPrefix('api/v1', { exclude: [''] });
 
   app.useGlobalPipes(
