1d25dd0722890d02c1edfce33e29634ec2d73ede
Quang Sơn Phạm <phamquangson4493@gmail.com>
2025-03-02 23:56:44 +0700

    fix config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,9 @@ import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true },);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const configService = app.get(ConfigService);
-  const port = process.env.VERCEL_URL
-    ? 3000
-    : configService.get('PORT') || 3000;
+  const port = configService.get('PORT') || 3000;
   app.setGlobalPrefix('api/v1', { exclude: [''] });
 
   app.useGlobalPipes(
